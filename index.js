f6fac8d5c753e5a324aa8c08bfa39b9c9bc7a5a8
jun9130 <jun.nakama1030@gmail.com>
2021-01-31 15:53:26 +0900

    v-bind

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = new Vue({
   el: '#app',
   data: {
-    url: 'https://www.yahoo.co.jp/'
+    url: 'https://www.yahoo.co.jp/',
+    attribute: 'href',
+    twitterObject: {
+      href: 'https://twitter.com',
+      id: 19
+    }
   }
 })
