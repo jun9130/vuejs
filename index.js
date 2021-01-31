a2cf8b7a9c0431786b2e7131092ca93e57772e88
jun9130 <jun.nakama1030@gmail.com>
2021-01-31 11:11:52 +0900

    v-on

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const app = new Vue({
   el: '#app',
   data: {
-    weeks: ['月', '火', '水', '木', '金', '土', '日'],
-    memoLists: {title: '1月1日', text: 'あけましておめでとう'}
+    message: 'apple'
+  },
+  methods: {
+    UpperCase: function() {
+      this.message = this.message.toUpperCase()
+    },
+    LowerCase: function() {
+      this.message = this.message.toLowerCase()
+    }
   }
 })
