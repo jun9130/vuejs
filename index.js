c4ec24e33c384319edd6845e0d71c1f8c465bb20
jun9130 <jun.nakama1030@gmail.com>
2021-02-01 14:03:17 +0900

    v-model

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 const app = new Vue({
   el: '#app',
   data: {
-    number: 0,
-    x: 0,
-    y: 0
-  },
-  methods: { 
-    countUp: function(times) {
-      this.number += 1 * times
-    },
-    changeMousePosition: function(event) {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    },
-    myAlert() {
-      alert('アラート');
-    }
+    message: 'こんにちは'
   }
 })
