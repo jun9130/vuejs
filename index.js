e30aafe777bd36a739b5391bda0dcf1fa8ce2d97
jun9130 <jun.nakama1030@gmail.com>
2021-02-09 13:37:52 +0900

    スタイルオブジェクト

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const app = new Vue({
   el: '#app',
   data: {
-    isActive: true,
-    color: 'red',
-    bg: 'blue'
-  },
-  computed: {
-    classObject: function() {
-      return { 
-        red: this.isActive, 
-        'blue': !this.isActive 
-      }
-     }
-    }
+    textColor: "red",
+    bgcolor: "blue"
+  }
   })
