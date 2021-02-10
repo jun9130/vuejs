9609d991ea9df837b228687233abf672395b6219
jun9130 <jun.nakama1030@gmail.com>
2021-02-10 14:44:00 +0900

    v-on

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = new Vue({
   el: '#app',
   data: {
-    items: [
-      {title: '項目1', id: 1},
-      {title: '項目2', id: 2},
-      {title: '項目3', id: 3},
-    ]
-  }
+    counter: 0
+  },
+  methods: {
+    add: function() {
+      this.counter++
+    },
+  },
 })
