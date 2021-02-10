ab85ed2a8729e1fdf6925759155a7f3f4f746ae7
jun9130 <jun.nakama1030@gmail.com>
2021-02-10 14:36:50 +0900

    v-for

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'OK',
-    isChecked: true
+    items: [
+      {title: '項目1', id: 1},
+      {title: '項目2', id: 2},
+      {title: '項目3', id: 3},
+    ]
   }
 })
