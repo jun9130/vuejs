0ec9914d986c419a1143c4f00c97083cf4925c1a
jun9130 <jun.nakama1030@gmail.com>
2021-02-12 16:26:07 +0900

    v-for オブジェクト

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = new Vue({
   el: '#app',
   data: {
-    fruits: ["ばなな", "みかん", "いちご"]
+    fruits: ["ばなな", "みかん", "いちご"],
+    object: {
+      firstName: "太郎",
+      lastName: "田中",
+      age: 23
+    }
   }
 })
