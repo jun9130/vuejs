06c7153fb4b57b186bdc1bf7858e775e41285d78
jun9130 <jun.nakama1030@gmail.com>
2021-02-15 14:13:34 +0900

    lesson

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: "OK"
-  }
-})
+    message: "OK",
 
-// app.message = "書き換えられました"
-
-const app2 = new Vue({
-  el: '#app2',
-  data: {
-    message: "こんにちは"
-  },
-  methods: {
-    changeMessage1: function() {
-      // 外部からappにアクセス
-      app.message = 'インスタンス2から変えました'
-    }
   }
 })
