e65331f21c0c09a2bf67e1b38c36d5bad9f1ab83
jun9130 <jun.nakama1030@gmail.com>
2021-02-16 13:38:18 +0900

    $data

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const data = {
+  message: "OK",
+  name: "ito"
+}
+
 const app = new Vue({
   el: '#app',
-  data: {
-    message: "OK",
-
-  }
+  data: data
 })
+
+
+console.log(data);
+
+console.log(data === app.$data);
