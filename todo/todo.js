992353a5a391c64886b56abd4512d86fc9b118cc
jun9130 <jun.nakama1030@gmail.com>
2021-02-18 13:48:45 +0900

    todoアプリ

---
+++ todo/todo.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    items: [
+      { title: '掃除をする', isChecked: true },
+      { title: 'ランニング', isChecked: true },
+      { title: '買い物に行く', isChecked: true }
+    ],
+    newItemTitle: ""
+  },
+  methods: {
+    addTodo: function() {
+      this.items.push({
+        title: this.newItemTitle,
+        isChecked: false
+      });
+      this.newItemTitle = "";
+    },
+    deleteTodo: function(){
+      this.items = this.items.filter(function (item) {
+        return item.isChecked === false;
+      });
+    },
+  }
+})
