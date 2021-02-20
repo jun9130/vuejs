fe81e8180e340749482afd003d90c952143837a8
jun9130 <jun.nakama1030@gmail.com>
2021-02-20 22:15:02 +0900

    Vueインスタンス ライフサイクル

--- index.js
+++ index.js
@@ -41,3 +41,40 @@ const div = document.createElement('div');
 console.log(div);
 console.log(document);
 console.dir(document);
+
+
+new Vue({
+  el: '#app4',
+  data: {
+    name: '山本'
+  },
+  beforeCreate: function() {
+    console.log('beforeCreate')
+  },
+  created: function() {
+    console.log('Created')
+  },
+  beforeMount: function() {
+    console.log('beforeMount')
+  },
+  mounted: function() {
+    console.log('mounted')
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate')
+  },
+  Updated: function() {
+    console.log('Updated')
+  },
+  beforeDestroy: function() {
+    console.log('beforeDestroy')
+  },
+  destroyed: function() {
+    console.log('destroyed')
+  },
+  methods: {
+    destroy: function(){
+      this.$destroy()
+    }
+  }
+})
