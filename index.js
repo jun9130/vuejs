f3a3c777e5bf94e081f41841097f9881acb2a33f
jun9130 <jun.nakama1030@gmail.com>
2021-02-22 13:47:41 +0900

    コンポーネント

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ Vue.component('my-conmponent', {
       number: 12
     }
   },
-  template: '<p>いいね{{number}}</p>'
+  template: '<p>いいね{{number}} <button v-on:click="increment">+1</button></p>',
+  methods: {
+    increment: function() {
+      this.number += 1;
+    }
+  }
 })
 
 
