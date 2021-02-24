2e225f0afd5a098fce76f546811e0d3335414ddc
jun9130 <jun.nakama1030@gmail.com>
2021-02-24 13:56:29 +0900

    ローカル登録、グローバル登録

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Vue.component('my-conmponent', {
+const component = {
   data: function(){
     return{
       number: 12
@@ -10,10 +10,32 @@ Vue.component('my-conmponent', {
       this.number += 1;
     }
   }
-})
+}
+
+
+// Vue.component('my-conmponent', {
+//   data: function(){
+//     return{
+//       number: 12
+//     }
+//   },
+//   template: '<p>いいね{{number}} <button v-on:click="increment">+1</button></p>',
+//   methods: {
+//     increment: function() {
+//       this.number += 1;
+//     }
+//   }
+// })
 
 
 const app = new Vue({
   el: '#app',
+  components: {
+    'my-conmponent': component
+  }
+})
+
+const app2 = new Vue({
+  el: '#app2',
  
 })
