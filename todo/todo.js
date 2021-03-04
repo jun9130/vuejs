641ceebf1ec536ce09a79dbfb79df8456b84f401
jun9130 <jun.nakama1030@gmail.com>
2021-03-04 14:08:06 +0900

    lesson

--- todo/todo.js
+++ todo/todo.js
@@ -1,25 +1,32 @@
-const app = new Vue({
-  el: '#app',
+// const app = new Vue({
+//   el: '#app',
+//   data: {
+//     items: [
+//       { title: '掃除をする', isChecked: true },
+//       { title: 'ランニング', isChecked: true },
+//       { title: '買い物に行く', isChecked: true }
+//     ],
+//     newItemTitle: ""
+//   },
+//   methods: {
+//     addTodo: function() {
+//       this.items.push({
+//         title: this.newItemTitle,
+//         isChecked: false
+//       });
+//       this.newItemTitle = "";
+//     },
+//     deleteTodo: function(){
+//       this.items = this.items.filter(function (item) {
+//         return item.isChecked === false;
+//       });
+//     },
+//   }
+// })
+
+const vm = new Vue({
+  el: '#demo',
   data: {
-    items: [
-      { title: '掃除をする', isChecked: true },
-      { title: 'ランニング', isChecked: true },
-      { title: '買い物に行く', isChecked: true }
-    ],
-    newItemTitle: ""
-  },
-  methods: {
-    addTodo: function() {
-      this.items.push({
-        title: this.newItemTitle,
-        isChecked: false
-      });
-      this.newItemTitle = "";
-    },
-    deleteTodo: function(){
-      this.items = this.items.filter(function (item) {
-        return item.isChecked === false;
-      });
-    },
+    show: true
   }
 })
