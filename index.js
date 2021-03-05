9366efea83d6f22f0b998c119191339f0b0a431f
jun9130 <jun.nakama1030@gmail.com>
2021-03-05 14:03:29 +0900

    v-model

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
-const component = {
-  data: function(){
-    return{
-      number: 12
-    }
-  },
-  template: '<p>いいね{{number}} <button v-on:click="increment">+1</button></p>',
-  methods: {
-    increment: function() {
-      this.number += 1;
-    }
-  }
-}
-
-
-// Vue.component('my-conmponent', {
-//   data: function(){
-//     return{
-//       number: 12
-//     }
-//   },
-//   template: '<p>いいね{{number}} <button v-on:click="increment">+1</button></p>',
-//   methods: {
-//     increment: function() {
-//       this.number += 1;
-//     }
-//   }
-// })
-
-
 const app = new Vue({
   el: '#app',
-  components: {
-    'my-conmponent': component
+  data: {
+    isActive: true
   }
-})
-
-const app2 = new Vue({
-  el: '#app2',
- 
 })
