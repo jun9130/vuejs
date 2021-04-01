766b5235f79f348054c6977441c7b0edf20d1526
jun9130 <jun.nakama1030@gmail.com>
2021-04-01 14:41:43 +0900

    コンポーネント

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+Vue.component('date', {
+  data: function() {
+    return {
+      today: '1999/04/01'
+    }
+  },
+  mounted: function() {
+    const now = new Date();
+    this.today = `${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()}`
+  },
+  template: '<div>{{ today }}</div>'
+
+})
+
 const app = new Vue({
-  el: '#app',
-  data: {
-    counter: 0
-  },
-  methods: {
-    add: function() {
-      this.counter++
-    },
-  },
+  el: "#app",
 })
